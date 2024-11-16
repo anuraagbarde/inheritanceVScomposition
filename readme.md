7c3fec82a43505079cd748f92390b0077bd0114b
Anuraag Barde <anuraagbarde@gmail.com>
2024-11-17 02:21:59 +0530

    added readme, start.sh

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Python OOP
+
+```bash
+chmod +x start.sh
+./start.sh
+```
---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "Inheritance\n"
+python3 ./inheritance.py
+
+echo "\n\n"
+
+echo "Composition\n"
+python3 ./composition.py
